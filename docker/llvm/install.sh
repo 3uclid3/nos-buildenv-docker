28df8fb74e615d556f042228411fe4145e871a0c
3uclid3 <3uclid3@pm.me>
2024-06-09 23:35:03 -0400

    Removed GCC and focus the image for LLVM

--- docker/llvm/install.sh
+++ docker/llvm/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SHORT=l:g:
-LONG=llvm:,gcc:
+SHORT=l:
+LONG=llvm:
 OPTS=$(getopt -o $SHORT --long $LONG -- "$@")
 
 eval set -- "$OPTS"
@@ -12,7 +12,6 @@ GCC=
 while true; do
   case "$1" in
     -l | --llvm ) LLVM="$2"; shift 2 ;;
-    -g | --gcc ) GCC="$2"; shift 2 ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -23,11 +22,6 @@ if [ -z "$LLVM" ]; then
   exit 1
 fi
 
-if [ -z "$GCC" ]; then
-  echo "-g | --gcc version is undefined"
-  exit 1
-fi
-
 echo "Updating PATH for pipx installed tools"
 export PATH="/root/.local/bin:$PATH"
 echo "PATH=$PATH"
@@ -35,8 +29,7 @@ echo "PATH=$PATH"
 echo "Installing packages from standard repositories"
 apt-get update -qq && export DEBIAN_FRONTEND=noninteractive && \
     apt-get install -y --no-install-recommends software-properties-common apt-utils wget curl file gpg \
-        build-essential ccache pkg-config git python3 python3-pip pipx xorriso qemu-system-x86 qemu-system-arm \
-        gcc-"$GCC" g++-"$GCC"
+        build-essential ccache pkg-config git python3 python3-pip pipx xorriso qemu-system-x86 qemu-system-arm
 
 echo "Installing packages from pip"
 pipx install cmake
@@ -52,17 +45,17 @@ echo "Installing LLVM"
 wget https://apt.llvm.org/llvm.sh
 chmod +x llvm.sh
 ./llvm.sh "$LLVM" all
-
-# Clean up
 rm llvm.sh
 
-echo "Verify installations"
-#cmake --version
-#meson --version
-#ninja --version
-#gcovr --version
-#xbstrap --version
-#gcc-"$GCC" --version
-#g++-"$GCC" --version
-#clang-"$LLVM" --version
-#clang++-"$LLVM" --version
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-"$LLVM" 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-"$LLVM" 100
+
+update-alternatives --install /usr/bin/lld lld /usr/bin/lld-"$LLVM" 100
+
+update-alternatives --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-"$LLVM" 100
+update-alternatives --install /usr/bin/llvm-strip llvm-strip /usr/bin/llvm-strip-"$LLVM" 100
+
+update-alternatives --install /usr/bin/clang-tidy clang-tidy /usr/bin/clang-tidy-"$LLVM" 100
+
+update-alternatives --install /usr/bin/cc cc /usr/bin/clang-"$LLVM" 100
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-"$LLVM" 100
